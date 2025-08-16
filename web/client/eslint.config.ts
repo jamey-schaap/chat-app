0c39ec507294adfbb8b29acc4b55df8c08fb86cc
Jamey Schaap <jameyschaap@proton.me>
2025-08-16 12:15:32 +0200

    Add prettier

--- web/client/eslint.config.ts
+++ web/client/eslint.config.ts
@@ -2,18 +2,25 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config([
-    {
-        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: {js}, extends: [{
-            name: "js/recommended"
-        }], languageOptions: {globals: globals.browser}
-    },
-    tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    {
-        rules: {
-            "react/react-in-jsx-scope": "off"
-        }
-    }
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		plugins: { js },
+		extends: [
+			{
+				name: "js/recommended",
+			},
+		],
+		languageOptions: { globals: globals.browser },
+	},
+	tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	eslintPluginPrettierRecommended,
+	{
+		rules: {
+			"react/react-in-jsx-scope": "off",
+		},
+	},
 ]);
